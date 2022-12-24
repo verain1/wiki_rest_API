1db4a0ae0a1c4d9933f37d041126868b5a07e2d4
Verain <verainthakur1183@gmail.com>
2022-12-25 02:20:00 +0530

    Added server code

--- webapp/urls.py
+++ webapp/urls.py
@@ -2,6 +2,5 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('/', homePageView),
     path('/<str:id>', ansPageView)
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -5,13 +5,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-dict_temp = {
-    'loc': 'Punjab',
-    'cap': 'Chandigarh'
-}
 
-def homePageView(request):
-    return HttpResponse(json.dumps(dict_temp))
+
+# def homePageView(request):
+#     return HttpResponse(json.dumps(dict_temp))
 
 def ansPageView(request, id):
     return HttpResponse(id)
